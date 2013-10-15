d0c4a72e1b7164b4e7b35369b237718e226c294e
Nguyen Bao <nguyenbao@Nguyens-Mac.local>
2013-10-14 18:27:26 -0700

    first one commit

---
+++ day6/main.m
@@ -0,0 +1,51 @@
+//
+//  main.m
+//  day6
+//
+//  Created by Nguyen Bao on 10/14/13.
+//  Copyright (c) 2013 Nguyen Quoc Bao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        //NSLog(@"Hello, World!");
+        
+        printf("\n");
+        printf("------rich rac-------");
+        
+        
+        printf("\n");
+        
+       printf("     .       .\n");
+       printf("    . .     . .\n");
+       printf("   .   .   .   .\n");
+       printf("  .     . .     .\n");
+       printf(" .       .       .\n");
+        
+        
+        printf("\n");
+        printf("------o to-------\n");
+        
+        printf("            .............                    \n");
+        printf("           .             .                   \n");
+        printf("          .               .                  \n");
+        printf("       ...                 ........          \n");
+        printf("     .    .....           .....     .        \n");
+        printf("     .   . ...  .        . ...  .     .      \n");
+        printf("     .. . .   .  ........ .    . ......      \n");
+        printf("          .   .           .   .              \n");
+        printf("           ...             ...               \n");
+        printf("                                             \n");
+        
+        printf("\n");
+        
+    }
+    return 0;
+}
+
